9b8033c909e9be735ccda91bda14b7398d5035f2
Raphael Beer <rbeerdev@gmail.com>
2016-03-21 23:58:05 +0100

    Insert link on success

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -8,8 +8,25 @@
   define('plugins/expiring-uploads/controller', deps, (controls) => {
     var controller = {};
     controller.validateUpload = (response, composer) => {
-      let insertText = '[' + response.linkText + '](' + response.url + ') ';
-      controls.insertIntoTextarea(composer.textarea, insertText);
+      let selAfterInsert = 0;
+      let insertText = '';
+      if (composer.selectionStart === composer.selectionEnd) {
+        insertText = '[' + response.linkText + '](' + response.url + ')';
+        selAfterInsert = composer.selectionEnd + insertText.length;
+        controls.insertIntoTextarea(composer.textarea, insertText);
+      } else {
+        insertText = '](' + response.url + ')';
+        selAfterInsert = composer.selectionEnd + response.url.length + 4;
+        controls.wrapSelectionInTextareaWith(composer.textarea, '[', insertText);
+      }
+      /**
+       * Modal timeout takes the focus from textarea if set too early
+       * https://github.com/nodebb/nodebb/blob/master/public/src/modules/uploader.js#L85-L88
+       */
+      setTimeout(() => controls.updateTextareaSelection(composer.textarea,
+                                                        selAfterInsert,
+                                                        selAfterInsert),
+                 1000);
     };
 
     return controller;
