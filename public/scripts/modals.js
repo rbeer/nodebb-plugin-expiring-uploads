9fe675d5ebc7a7025d55e8fb6fd1379a8e3a2b20
Raphael Beer <rbeerdev@gmail.com>
2016-03-22 03:35:04 +0100

    Add translation to upload modal

--- public/scripts/modals.js
+++ public/scripts/modals.js
@@ -6,21 +6,26 @@
   let deps = [
     'uploader',
     'plugins/expiring-uploads/controller',
-    'plugins/expiring-uploads/sockets'
+    'plugins/expiring-uploads/sockets',
+    'translator'
   ];
-  define('plugins/expiring-uploads/modals', deps, (uploader, controller, sockets) => {
+  define('plugins/expiring-uploads/modals', deps, (uploader, controller, sockets, translator) => {
     var modals = {};
     modals.showUploadModal = function(composer) {
       sockets.getUploadModalSettings((err, settings) => {
         if (err) return console.error('Error getting settings from backend');
-        uploader.show({
-          route: config.relative_path + '/plugins/expiring-uploads/upload',
-          params: {},
-          fileSize: config.maximumFileSize,
-          title: '[[user:upload_picture]]',
-          description: '[[user:upload_a_picture]]',
-          accept: settings.expiringTypes
-        }, (response) => controller.validateUpload(response, composer));
+        let translateKeys = '[[expiringuploads:modal.title]];[[expiringuploads:modal.description]]';
+        translator.translate(translateKeys, (translated) => {
+          let strings = translated.split(';');
+          uploader.show({
+            route: config.relative_path + '/plugins/expiring-uploads/upload',
+            params: {},
+            fileSize: config.maximumFileSize,
+            title: strings[0],
+            description: strings[1],
+            accept: settings.expiringTypes
+          }, (response) => controller.validateUpload(response, composer));
+        });
       });
     };
     return modals;
