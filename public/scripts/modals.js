7ec7e6fb9790ffda5f8ffb1f791e78697718b2d9
Raphael Beer <rbeerdev@gmail.com>
2016-03-22 06:34:42 +0100

    Hoist upload modal less intrusive over fullscreen composer

--- public/scripts/modals.js
+++ public/scripts/modals.js
@@ -5,11 +5,11 @@
 
   let deps = [
     'uploader',
+    'translator',
     'plugins/expiring-uploads/controller',
-    'plugins/expiring-uploads/sockets',
-    'translator'
+    'plugins/expiring-uploads/sockets'
   ];
-  define('plugins/expiring-uploads/modals', deps, (uploader, controller, sockets, translator) => {
+  define('plugins/expiring-uploads/modals', deps, (uploader, translator, controller, sockets) => {
     var modals = {};
 
     modals.showUploadModal = function(composer) {
@@ -18,6 +18,9 @@
         let translateKeys = '[[expiringuploads:modal.title]];[[expiringuploads:modal.description]]';
         translator.translate(translateKeys, (translated) => {
           let strings = translated.split(';');
+
+          (new MutationObserver(hoistModal)).observe(document.body, {childList: true});
+
           uploader.show({
             route: config.relative_path + '/plugins/expiring-uploads/upload',
             params: {},
@@ -27,8 +30,23 @@
                                          expireString(settings.expireAfter)),
             accept: settings.expiringTypes
           }, (response) => controller.validateUpload(response, composer));
+
         });
       });
+    };
+
+    const hoistModal = (mutations, observer) => {
+      let addedNodes = mutations.map((mutation) => mutation.addedNodes[0]);
+      let target = addedNodes.filter(targetNodeFilter)[0];
+      if (target) {
+        target.classList.add('hoisted');
+        observer.disconnect();
+      }
+    }
+    const targetNodeFilter = (node) => {
+      if (!node || !node.classList.contains('modal')) return false;
+      let aria = node.attributes.getNamedItem('aria-labelledby');
+      return (aria || {value: 0}).value === 'upload-file';
     };
 
     const expireString = (expireAfter) => {
